b63678828aad559728fafc3220413336eedaa634
Lucas Araujo <lsa1@live.com.ar>
2023-05-29 22:31:39 -0300

    correccion de sintaxis

--- finanscript.py
+++ finanscript.py
@@ -160,7 +160,6 @@ while True:
                 balance = Balance(TotalSell, total)
                 print(f"Balance: ${balance}")
                 ToExcel_Op("Balance", 5, "=N2-M2", 6, 13)
-
                 Totalspect = Mas(balance, TotalCharges)
                 print(f"Balance mas total de encargos: ${Totalspect}")
                 ToExcel_Op("Balance mas total de encargos", 5, "=O2+M6", 6, 15)
